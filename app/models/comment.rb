4c307fbaa2c1616ff9e98430ae950865f5463662
yudai <yudaikawabuchi@gmai.com>
2020-01-31 22:02:51 +0900

    modify gitignore

--- app/models/comment.rb
+++
@@ -1,4 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :micropost
-  validates :content, presence: true
-end
--- db/migrate/20200127214635_add_index_comments_micropost_id.rb
+++
@@ -1,6 +0,0 @@
-class AddIndexCommentsMicropostId < ActiveRecord::Migration[6.0]
-
-  def change
-  end
-  
-end
